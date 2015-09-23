6a15aabb16bfbf5a23b2feadc45d7a5527b00886
Danilo Recchia <danilo@deltatecnologia.com>
2015-09-23 16:03:39 -0300

    Update runme.sh

--- files/runme.sh
+++ files/runme.sh
@@ -1,3 +1,7 @@
+chown -R collabnet.collabnet /opt/csvn
+# I know, just in case if this is a mapped volume
 chown -R collabnet.collabnet /opt/csvn/data
+
+cd /opt/csvn 
 bin/csvn start
 tail -f /dev/null
